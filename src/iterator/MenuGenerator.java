f5313ac8c16e06f5eee7cb6b6a1c1cc9963db459
Huy Ho <tuanhuy0496@yahoo.com.vn>
2020-04-09 22:55:35 +0700

    Add Exit option to Menu

--- src/iterator/MenuGenerator.java
+++ src/iterator/MenuGenerator.java
@@ -6,11 +6,12 @@ import java.util.List;
 public class MenuGenerator {
     public static void toShow() {
         List<String> menus = new ArrayList<>();
-        menus.add("To use this system, there is a list option below");
+        menus.add("To use this system, choose options below");
         menus.add("1) Create");
         menus.add("2) Read");
         menus.add("3) Update");
         menus.add("4) Delete");
+        menus.add("0) Exit");
 
         MenuList menuList = new MenuList();
         menuList.menus = menus;
