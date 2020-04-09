943512db638b1f47061e5e6eea82dcc147a89376
Huy Ho <tuanhuy0496@yahoo.com.vn>
2020-04-09 18:17:20 +0700

    Add Junit Testing for Builder

---
+++ src/builder/TestBuilder.java
@@ -0,0 +1,23 @@
+package builder;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import sample.Courses;
+import sample.Students;
+
+public class TestBuilder {
+    @Test
+    public void testCourseBuilder(){
+        Courses course = new Courses();
+        course.add(new CourseBuilder().id("COSC2440").name("Software Architecture Design and Implementation").numberOfCredits(12).build());
+        Assert.assertEquals("ID: COSC2440, Name: Software Architecture Design and Implementation, Credit: 12", course);
+    }
+    @Test
+    public void testStudentBuilder(){
+        Students students = new Students();
+        students.add(new StudentBuilder().id("s3756868").name("Ho Tuan Huy").age(23).build());
+        Assert.assertEquals("ID: s3756868, Name: Ho Tuan Huy, Age: 23", students);
+    }
+
+}
